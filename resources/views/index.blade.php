d46f988627bd589379aeceffadb1efff445d0811
rmtar <picpoint@yandex.ru>
2021-06-02 13:27:26 +0300

    work with layout home page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -30,14 +30,97 @@
                 <div class="hp__hdrbuttom">
                     <div class="hp__hdrbuttomwrap">
                         <div class="hp__hdrbuttomlogo">
-
+                            <img src="/public/pict/comp.png" alt="logo">
                         </div>
                         <div class="hp__hdrbuttomfind">
-
+                            <form method="post" class="hp__find">
+                                <input type="text" name="find" placeholder="Поиск по сайту">
+                            </form>
                         </div>
                         <div class="hp__hdrbuttomother">
-                            
+                            <div class="hp__hdrbuttombasket">
+                                <a href="#">Корзина</a>
+                            </div>
+                            <div class="hp__hdrbuttomauth">
+                                <a href="#">Войти</a>
+                            </div>
                         </div>
+                    </div>
+                </div>
+            </div>
+            <div class="hp__bdyblock">
+                <div class="hp__dbyblockwrap">
+                    <div class="hp__cpu">
+                        <a href="#">
+                            <img src="/public/pict/cpu.png" alt="cpu">
+                            <span>Процессоры</span>
+                        </a>
+                    </div>
+                    <div class="hp__mbord">
+                        <a href="#">
+                            <img src="/public/pict/mboard.png" alt="mboard">
+                            <span>Метеринские платы</span>
+                        </a>
+                    </div>
+                    <div class="hp__video">
+                        <a href="#">
+                            <img src="/public/pict/video.png" alt="video">
+                            <span>Видеокарты</span>
+                        </a>
+                    </div>
+                    <div class="hp__ram">
+                        <a href="#">
+                            <img src="/public/pict/ram.png" alt="ram">
+                            <span>Оперативная память</span>
+                        </a>
+                    </div>
+                    <div class="hp__power">
+                        <a href="#">
+                            <img src="/public/pict/power.png" alt="power">
+                            <span>Блоки питания</span>
+                        </a>
+                    </div>
+                    <div class="hp__corps">
+                        <a href="#">
+                            <img src="/public/pict/corp.png" alt="corps">
+                            <span>Корпуса</span>
+                        </a>
+                    </div>
+                    <div class="hp__cooling">
+                        <a href="#">
+                            <img src="/public/pict/cooling.png" alt="cooling">
+                            <span>Охлаждение</span>
+                        </a>
+                    </div>
+                    <div class="hp__modding">
+                        <a href="#">
+                            <img src="/public/pict/modding.png" alt="modding">
+                            <span>Обслуживание</span>
+                        </a>
+                    </div>
+                    <div class="hp__ssd">
+                        <a href="#">
+                            <img src="/public/pict/ssd.png" alt="ssd">
+                            <span>SSD Накопители</span>
+                        </a>
+                    </div>
+                    <div class="hp__hdd">
+                        <a href="#">
+                            <img src="/public/pict/hdd.png" alt="hdd">
+                            <span>HDD Накопители</span>
+                        </a>
+                    </div>
+                    <div class="hp__expdevices">
+                        <a href="#">
+                            <img src="/public/pict/exp.png" alt="exp">
+                            <span>Устройства расширения</span>
+                        </a>
+                    </div>
+                    <div class="hp__accessories">
+                        <a href="#">
+                            <img src="/public/pict/access.png" alt="accessories">
+                            <span>Комплектующие</span>
+                        </a>
                     </div>
                 </div>
             </div>
